d7105812d1788371c5e5509b6ce9f88ae5529e5a
minikermit <eyesonly202@hotmail.com>
2010-12-05 21:08:02 +0100

    jquery autocomplete example

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+
+  autocomplete :brand, :name
+
+  def show
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails3JqueryAutocomplete::Application.config.secret_token = '46e032a3de95621c4de4cdacd0332b1c26f13bb8a71aa7fd167c09187d1181e8befb8a9f91a55eb50c45ae802e576da090fb5ad459895ca6f5438378bdffa721'
